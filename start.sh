02d83c52f9398ec999dc324a26ff590dd4313ab6
truyk87 <133194606+truyk87@users.noreply.github.com>
2023-05-11 06:03:03 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=e4c40e74-8352-4bba-a36c-10349334eba4
+export UUID=805f1eb7-1aa8-4361-8400-e69ef5dfe16e
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=techtanker.com
+export ProxySite=hoop-light.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
